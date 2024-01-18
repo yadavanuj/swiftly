eb1b907c03447d6ae206670d04fc572e947ed583
yadavanuj <anujyadavcse@gmail.com>
2024-01-18 09:43:42 +0530

    Little refactoring

--- src/ui/Swiftly.js
+++ src/ui/Swiftly.js
@@ -1,43 +1,8 @@
 import React from "react";
 import { SwiftlyInput } from "./SwiftlyInput";
 import { jsonSchema, viewProps } from "../schema/bulkhead";
+import { mergeDeep } from "../libs/not.perfect.merger";
 
-/**
- * Simple object check.
- * @param item
- * @returns {boolean}
- */
-export function isObject(item) {
-    return (item && typeof item === 'object' && !Array.isArray(item));
-  }
-  
-  /**
-   * Deep merge two objects.
-   * @param target
-   * @param ...sources
-   */
-  export function mergeDeep(target, sources) {
-    if (!sources.length) return target;
-
-    const __merge = (__target, __source) => {
-        if (isObject(__target) && isObject(__source)) {
-            for (const key in __source) {
-              if (isObject(__source[key])) {
-                if (!__target[key]) Object.assign(__target, { [key]: {} });
-                __merge(__target[key], __source[key]);
-              } else {
-                Object.assign(__target, { [key]: __source[key] });
-              }
-            }
-          }
-    }
-
-    for (const key in sources) {
-        __merge(target, sources[key]);
-    }
-
-    return target;
-  }
 
 const properties = {};
 mergeDeep(properties, [jsonSchema.properties, viewProps.properties]);
@@ -64,6 +29,9 @@ export function Swiftly() {
 /**
  * Read schema
  * Apply validation checks in composable manner
- * Manage state
+ * Manage state (...?)
+ * 
  */
-class SwiftlyCore {}
+class SwiftlyCore {
+
+}
